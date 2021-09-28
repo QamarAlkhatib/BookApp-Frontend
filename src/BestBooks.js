ddb664b06930df23704d191d2ba8f4af228f14c8
Qamar <qalkhatib0@gmail.com>
2021-09-28 04:17:23 +0300

    adding data mongodb

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -2,16 +2,67 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import './BestBooks.css';
-
+import axios from 'axios';
+// import { withAuth0 } from "@auth0/auth0-react";
+import { Card, CardGroup } from 'react-bootstrap/'
 class MyFavoriteBooks extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookData: [],
+      
+    }
+}
+
+
+ componentDidMount = async () =>{
+    // const { user,isAuthenticated} = withAuth0();
+    console.log('mount function');
+
+    let books =  await axios.get(`${process.env.REACT_APP_SERVER}/books`);
+    this.setState({
+      bookData: books.data
+      
+    });
+    console.log("dattttaa", this.state.bookData);
+  }
+
+
+
   render() {
-    return(
-      <Jumbotron>
-        <h1>My Favorite Books</h1>
-        <p>
-          This is a collection of my favorite books
-        </p>
-      </Jumbotron>
+
+    return (
+      <>
+        <Jumbotron>
+          <h1>My Favorite Books</h1>
+          <p>
+            This is a collection of my favorite books
+          </p>
+        </Jumbotron>
+        {this.state.bookData &&
+          <>
+            {this.state.bookData.map(element => {
+              return (
+                <>
+                  <CardGroup>
+                    <Card>
+                      <Card.Body>
+                        <Card.Title>{element.title}</Card.Title>
+                        <Card.Text>
+                          {element.description}
+                          <br></br>
+                          {element.email}
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </CardGroup>
+                </>
+              )
+            })}
+          </>
+        }
+      </>
     )
   }
 }
